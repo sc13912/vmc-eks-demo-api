cbc6d8652a0bed96bfbab0aed62218b0b2b48b19
Cindy Xu <cindy.xu@boral.com.au>
2021-04-19 20:59:44 +1000

    remove scripts

--- initTable.sql
+++
@@ -1,7 +0,0 @@
---  psql -h localhost -U guestdba -d guestdb  -f initTable.sql
-CREATE TABLE IF NOT EXISTS guest (
-   id Integer PRIMARY KEY ,
-   name VARCHAR,
-   message VARCHAR,
-   created_on timestamp
-);
